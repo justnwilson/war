4e0fadf72d392aabe3da3cd045cdf7204da35f9d
justnwilson <justn.wilson@proton.me>
2025-01-24 19:01:27 -0700

    fixes facing card problem, assigns proper spriteIndexes

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ local gameState = {
     shuffled = {},
     player_plays = {},
     npc_plays = {},
+    player_test = {{value = "14", suit = "Spades", spriteIndex = 39}},
     war_cards = {}, --face-down cards during war
     war_face = {}, --face-up cars during war
     deck = {
@@ -15,54 +16,54 @@ local gameState = {
         {value = "14", suit = "Hearts", spriteIndex = 1},  -- Ace of Hearts
         {value = "14", suit = "Diamonds", spriteIndex = 20}, -- Ace of Diamonds
         {value = "14", suit = "Clubs", spriteIndex = 58},   -- Ace of Clubs
-        {value = "13", suit = "Spades", spriteIndex = 5},   -- King of Spades
-        {value = "13", suit = "Hearts", spriteIndex = 6},   -- King of Hearts
-        {value = "13", suit = "Diamonds", spriteIndex = 7}, -- King of Diamonds
-        {value = "13", suit = "Clubs", spriteIndex = 8},   -- King of Clubs
-        {value = "12", suit = "Spades", spriteIndex = 9},   -- Queen of Spades
-        {value = "12", suit = "Hearts", spriteIndex = 10},  -- Queen of Hearts
-        {value = "12", suit = "Diamonds", spriteIndex = 11}, -- Queen of Diamonds
-        {value = "12", suit = "Clubs", spriteIndex = 12},  -- Queen of Clubs
-        {value = "11", suit = "Spades", spriteIndex = 13},  -- Jack of Spades
-        {value = "11", suit = "Hearts", spriteIndex = 14},  -- Jack of Hearts
-        {value = "11", suit = "Diamonds", spriteIndex = 15}, -- Jack of Diamonds
-        {value = "11", suit = "Clubs", spriteIndex = 16},  -- Jack of Clubs
-        {value = "10", suit = "Spades", spriteIndex = 17}, -- 10 of Spades
-        {value = "10", suit = "Hearts", spriteIndex = 18}, -- 10 of Hearts
-        {value = "10", suit = "Diamonds", spriteIndex = 19}, -- 10 of Diamonds
-        {value = "10", suit = "Clubs", spriteIndex = 20},  -- 10 of Clubs
-        {value = "9", suit = "Spades", spriteIndex = 21},  -- 9 of Spades
-        {value = "9", suit = "Hearts", spriteIndex = 22},  -- 9 of Hearts
-        {value = "9", suit = "Diamonds", spriteIndex = 23}, -- 9 of Diamonds
-        {value = "9", suit = "Clubs", spriteIndex = 24},  -- 9 of Clubs
-        {value = "8", suit = "Spades", spriteIndex = 25},  -- 8 of Spades
-        {value = "8", suit = "Hearts", spriteIndex = 26},  -- 8 of Hearts
+        {value = "13", suit = "Spades", spriteIndex = 51},   -- King of Spades
+        {value = "13", suit = "Hearts", spriteIndex = 13},   -- King of Hearts
+        {value = "13", suit = "Diamonds", spriteIndex = 32}, -- King of Diamonds
+        {value = "13", suit = "Clubs", spriteIndex = 70},   -- King of Clubs
+        {value = "12", suit = "Spades", spriteIndex = 50},   -- Queen of Spades
+        {value = "12", suit = "Hearts", spriteIndex = 12},  -- Queen of Hearts
+        {value = "12", suit = "Diamonds", spriteIndex = 31}, -- Queen of Diamonds
+        {value = "12", suit = "Clubs", spriteIndex = 69},  -- Queen of Clubs
+        {value = "11", suit = "Spades", spriteIndex = 49},  -- Jack of Spades
+        {value = "11", suit = "Hearts", spriteIndex = 11},  -- Jack of Hearts
+        {value = "11", suit = "Diamonds", spriteIndex = 30}, -- Jack of Diamonds
+        {value = "11", suit = "Clubs", spriteIndex = 68},  -- Jack of Clubs
+        {value = "10", suit = "Spades", spriteIndex = 48}, -- 10 of Spades
+        {value = "10", suit = "Hearts", spriteIndex = 10}, -- 10 of Hearts
+        {value = "10", suit = "Diamonds", spriteIndex = 29}, -- 10 of Diamonds
+        {value = "10", suit = "Clubs", spriteIndex = 67},  -- 10 of Clubs
+        {value = "9", suit = "Spades", spriteIndex = 47},  -- 9 of Spades
+        {value = "9", suit = "Hearts", spriteIndex = 9},  -- 9 of Hearts
+        {value = "9", suit = "Diamonds", spriteIndex = 28}, -- 9 of Diamonds
+        {value = "9", suit = "Clubs", spriteIndex = 66},  -- 9 of Clubs
+        {value = "8", suit = "Spades", spriteIndex = 46},  -- 8 of Spades
+        {value = "8", suit = "Hearts", spriteIndex = 8},  -- 8 of Hearts
         {value = "8", suit = "Diamonds", spriteIndex = 27}, -- 8 of Diamonds
-        {value = "8", suit = "Clubs", spriteIndex = 28},  -- 8 of Clubs
-        {value = "7", suit = "Spades", spriteIndex = 29},  -- 7 of Spades
-        {value = "7", suit = "Hearts", spriteIndex = 30},  -- 7 of Hearts
-        {value = "7", suit = "Diamonds", spriteIndex = 31}, -- 7 of Diamonds
-        {value = "7", suit = "Clubs", spriteIndex = 32},  -- 7 of Clubs
-        {value = "6", suit = "Spades", spriteIndex = 33},  -- 6 of Spades
-        {value = "6", suit = "Hearts", spriteIndex = 34},  -- 6 of Hearts
-        {value = "6", suit = "Diamonds", spriteIndex = 35}, -- 6 of Diamonds
-        {value = "6", suit = "Clubs", spriteIndex = 36},  -- 6 of Clubs
-        {value = "5", suit = "Spades", spriteIndex = 37},  -- 5 of Spades
-        {value = "5", suit = "Hearts", spriteIndex = 38},  -- 5 of Hearts
-        {value = "5", suit = "Diamonds", spriteIndex = 39}, -- 5 of Diamonds
-        {value = "5", suit = "Clubs", spriteIndex = 40},  -- 5 of Clubs
-        {value = "4", suit = "Spades", spriteIndex = 41},  -- 4 of Spades
-        {value = "4", suit = "Hearts", spriteIndex = 42},  -- 4 of Hearts
-        {value = "4", suit = "Diamonds", spriteIndex = 43}, -- 4 of Diamonds
-        {value = "4", suit = "Clubs", spriteIndex = 44},  -- 4 of Clubs
-        {value = "3", suit = "Spades", spriteIndex = 45},  -- 3 of Spades
-        {value = "3", suit = "Hearts", spriteIndex = 46},  -- 3 of Hearts
-        {value = "3", suit = "Diamonds", spriteIndex = 47}, -- 3 of Diamonds
-        {value = "3", suit = "Clubs", spriteIndex = 48},  -- 3 of Clubs
-        {value = "2", suit = "Spades", spriteIndex = 49},  -- 2 of Spades
-        {value = "2", suit = "Hearts", spriteIndex = 50},  -- 2 of Hearts
-        {value = "2", suit = "Diamonds", spriteIndex = 51}, -- 2 of Diamonds
-        {value = "2", suit = "Clubs", spriteIndex = 52}   -- 2 of Clubs
+        {value = "8", suit = "Clubs", spriteIndex = 65},  -- 8 of Clubs
+        {value = "7", suit = "Spades", spriteIndex = 45},  -- 7 of Spades
+        {value = "7", suit = "Hearts", spriteIndex = 7},  -- 7 of Hearts
+        {value = "7", suit = "Diamonds", spriteIndex = 26}, -- 7 of Diamonds
+        {value = "7", suit = "Clubs", spriteIndex = 64},  -- 7 of Clubs
+        {value = "6", suit = "Spades", spriteIndex = 44},  -- 6 of Spades
+        {value = "6", suit = "Hearts", spriteIndex = 6},  -- 6 of Hearts
+        {value = "6", suit = "Diamonds", spriteIndex = 25}, -- 6 of Diamonds
+        {value = "6", suit = "Clubs", spriteIndex = 63},  -- 6 of Clubs
+        {value = "5", suit = "Spades", spriteIndex = 43},  -- 5 of Spades
+        {value = "5", suit = "Hearts", spriteIndex = 5},  -- 5 of Hearts
+        {value = "5", suit = "Diamonds", spriteIndex = 24}, -- 5 of Diamonds
+        {value = "5", suit = "Clubs", spriteIndex = 62},  -- 5 of Clubs
+        {value = "4", suit = "Spades", spriteIndex = 42},  -- 4 of Spades
+        {value = "4", suit = "Hearts", spriteIndex = 4},  -- 4 of Hearts
+        {value = "4", suit = "Diamonds", spriteIndex = 23}, -- 4 of Diamonds
+        {value = "4", suit = "Clubs", spriteIndex = 61},  -- 4 of Clubs
+        {value = "3", suit = "Spades", spriteIndex = 41},  -- 3 of Spades
+        {value = "3", suit = "Hearts", spriteIndex = 3},  -- 3 of Hearts
+        {value = "3", suit = "Diamonds", spriteIndex = 22}, -- 3 of Diamonds
+        {value = "3", suit = "Clubs", spriteIndex = 60},  -- 3 of Clubs
+        {value = "2", suit = "Spades", spriteIndex = 40},  -- 2 of Spades
+        {value = "2", suit = "Hearts", spriteIndex = 2},  -- 2 of Hearts
+        {value = "2", suit = "Diamonds", spriteIndex = 21}, -- 2 of Diamonds
+        {value = "2", suit = "Clubs", spriteIndex = 59}   -- 2 of Clubs
     }
 }
 
@@ -201,8 +202,8 @@ function love.update(dt)
     if spacePressed then
         spacePressed = false
         --pull top card from each player to compare
-        gameState.player_plays = table.remove(gameState.player_hand, 1)
-        gameState.npc_plays = table.remove(gameState.npc_hand, 1)
+        gameState.player_plays = { table.remove(gameState.player_hand, 1) }
+        gameState.npc_plays = { table.remove(gameState.npc_hand, 1) }
 
         --check if the players have enough cards
         if not gameState.player_plays then
@@ -216,20 +217,20 @@ function love.update(dt)
         end
 
         --print some info of the cards being played
-        for key, value in pairs(gameState.player_plays) do
+        for key, value in pairs(gameState.player_plays[1]) do
             print(key, value)
         end
-        for key, value in pairs(gameState.npc_plays) do
+        for key, value in pairs(gameState.npc_plays[1]) do
             print(key, value)
         end 
 
         --player wins
-        if gameState.player_plays.value > gameState.npc_plays.value then
+        if gameState.player_plays[1].value > gameState.npc_plays[1].value then
             table.insert(gameState.player_hand, gameState.player_plays)
             table.insert(gameState.player_hand, gameState.npc_plays)
 
         --npc wins
-        elseif gameState.npc_plays.value > gameState.player_plays.value then
+        elseif gameState.npc_plays[1].value > gameState.player_plays[1].value then
             table.insert(gameState.npc_hand, gameState.npc_plays)
             table.insert(gameState.npc_hand, gameState.player_plays)
 
@@ -241,34 +242,41 @@ function love.update(dt)
 end
 
 
+
 function love.draw()
     --cards in players hands
     love.graphics.print("Player's Hand: " .. #gameState.player_hand .. " cards", 50, 450)
     love.graphics.print("NPC's Hand: " .. #gameState.npc_hand .. " cards", 50, 100)
 
-
     --draw the players face-up card
-    if gameState.player_plays then
-        --BREAKS: when using the desired player_plays
-        --Errors here when player runs out of cards
-        love.graphics.draw(deckfile, quads[gameState.player_hand[1].spriteIndex], 300, 400)
+    if gameState.player_plays[1] then
+        love.graphics.draw(deckfile, quads[gameState.player_plays[1].spriteIndex], 300, 400)
+        love.graphics.print("Player plays: " .. gameState.player_plays[1].value .. " " .. gameState.player_plays[1].suit, 50, 470)
+
+    else
+        --not working yet
+        love.graphics.draw(deckfile, quads[77], 300, 400)
     end
 
-    --draw the npc's face-up card
-    if gameState.npc_plays then
-        --BREAKS: when using the desired npc_plays
-        --ERRORS here when npc runs out of cards
-        love.graphics.draw(deckfile, quads[gameState.npc_hand[1].spriteIndex], 300, 50)
+    --draw the npcs face-up card
+    if gameState.npc_plays[1] then
+        love.graphics.draw(deckfile, quads[gameState.npc_plays[1].spriteIndex], 300, 50)
+        love.graphics.print("NPC plays: " .. gameState.npc_plays[1].value .. " " .. gameState.npc_plays[1].suit, 50, 120)
+
+
+    else
+        --not working yet
+        love.graphics.draw(deckfile, quads[77], 300, 50)
     end
+end
 
     --draw the war_cards (face-down cards)
     --draw the war_face (face-up cards)
-    if #gameState.war_face > 0 then
+--[[     if #gameState.war_face > 0 then
         for i, card in ipairs(gameState.war_cards) do
             love.graphics.draw(deckfile, quads[card[1].spriteIndex], 200, 250 + i * 20)
         end
-    end
-end
+    end ]]
 
 
 
